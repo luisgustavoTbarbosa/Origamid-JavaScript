0f4df2b18de9bf5392c98ea1c0f205cb135c8614
Luis Gustavo <gustavobarbosag7@gmail.com>
2021-09-30 20:20:38 -0300

    variables and data types

---
+++ Tipos de dados/script.js
@@ -0,0 +1,28 @@
+var nome = 'luis';
+var sobreNome = 'gustavo';
+var nomeCompleto = nome + ' ' + sobreNome;
+
+console.log(nomeCompleto);
+
+// Declare uma variável contendo uma string
+var comida = 'Pizza';
+
+// Declare uma variável contendo um número dentro de uma string
+var codigoComida = '2';
+
+// Declare uma variável com a sua idade
+var idade = 19
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Luis';
+var segundoNome = 'Gustavo';
+var nomeCompleto = `${nome} ${segundoNome}`;
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = 'It\'s time';
+console.log(frase)
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
---
+++ variaveis/script.js
@@ -0,0 +1,34 @@
+/*
+var nome = "Luis";
+var idade = 19;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+*/
+
+//EXERCICIOS
+
+// Declarar uma variável com o seu nome
+var nome = 'Luís Gustavo';
+
+// Declarar uma variável com a sua idade
+var idade = 19;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza';
+
+console.log(nome, idade, comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+var endereco,
+    animalDeEstimação,
+    mae,
+    pai,
+    escola;
